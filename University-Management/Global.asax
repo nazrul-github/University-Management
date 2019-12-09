910951e40af9bf8adc1b0e31cd4e6b356e37dcdd
Robin <nozrulgithub@gmail.com>
2019-12-09 19:13:45 +0600

    Add project files.

---
+++ University-Management/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="University_Management.MvcApplication" Language="C#" %>
---
+++ University-Management/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(University_Management.Startup))]
+namespace University_Management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
