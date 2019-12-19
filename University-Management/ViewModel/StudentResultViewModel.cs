d7eb4e94617647285ae7e1aa15b3cb567c0fc5d5
Robin <nozrulgithub@gmail.com>
2019-12-19 23:51:25 +0600

    Working on making pdf for student result

---
+++ University-Management/ViewModel/StudentResultViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace University_Management.ViewModel
+{
+    public class ResultCourseView
+    {
+        public int CourseId { get; set; }
+        public string CourseCode { get; set; }
+        public string CourseName { get; set; }
+        public string Grade { get; set; }
+    }
+    public class StudentResultViewModel
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string StudentRegistrationNumber { get; set; }
+        public string StudentEmail { get; set; }
+        public string StudentDepartment { get; set; }
+        public List<ResultCourseView> ResultCourseViews { get; set; }
+    }
+}
